2f2a7adac801ec8eee765d64aa3c91fa370a63ce
Ellie <elliemmanifold@gmail.com>
2019-11-27 18:10:14 +0000

    Attack functionality

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'Testing name form' do
   scenario 'Can check players have entered names in a form, and names are shown on screen when form is submitted' do
-    visit('/')
-    fill_in "player_1_name", :with => 'Ellie'
-    fill_in "player_2_name", :with => 'Shaf'
-    click_on 'Submit'
+    sign_in_and_play
     expect(page).to have_content "Ellie vs. Shaf"
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,10 +2,12 @@ ENV['RACK_ENV'] = 'test'
 
 # require our Sinatra app file
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
+require 'features/web_helpers'
 
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
+
 
 # tell Capybara about our app class
 Capybara.app = Battle
