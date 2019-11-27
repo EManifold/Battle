328955e09f739b0890aa6d02d444ab089edcfb46
Ellie <elliemmanifold@gmail.com>
2019-11-27 16:00:17 +0000

    Refactor form to include two player names

--- app.rb
+++ app.rb
@@ -6,8 +6,9 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    @name = params[:name]
-    # erb(:play)
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+    erb(:play)
   end
 
   # start the server if ruby file executed directly
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -8,8 +8,9 @@
 feature 'Testing name form' do
   scenario 'Can check players have entered names in a form, and names are shown on screen when form is submitted' do
     visit('/')
-    fill_in "name", :with => 'Ellie'
+    fill_in "player_1_name", :with => 'Ellie'
+    fill_in "player_2_name", :with => 'Shaf'
     click_on 'Submit'
-    expect(page).to have_content 'Ellie'
+    expect(page).to have_content 'Ellie vs Shaf'
   end
 end
