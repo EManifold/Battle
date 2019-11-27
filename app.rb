d3de2f9f4f67756eaba43eee3431839b898f7ae1
Ellie <elliemmanifold@gmail.com>
2019-11-27 15:30:50 +0000

    Render form

--- app.rb
+++ app.rb
@@ -2,10 +2,13 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
   get '/' do
-    'Testing infrastructure working!'
+    erb(:index)
   end
 
-
+  post '/names' do
+    @name = params[:name]
+    # erb(:play)
+  end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,6 +1,15 @@
-feature 'Testing infrastructure' do
-  scenario 'Can check app is working and content showing' do
+# feature 'Testing infrastructure' do
+#   scenario 'Can check app is working and content showing' do
+#     visit('/')
+#     expect(page).to have_content 'Testing infrastructure working!'
+#   end
+# end
+
+feature 'Testing name form' do
+  scenario 'Can check players have entered names in a form, and names are shown on screen when form is submitted' do
     visit('/')
-    expect(page).to have_content 'Testing infrastructure working!'
+    fill_in "name", :with => 'Ellie'
+    click_on 'Submit'
+    expect(page).to have_content 'Ellie'
   end
 end
